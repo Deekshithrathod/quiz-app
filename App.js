36e7ec2a69011889612ab0a594974db775e3b0f3
Deekshithrathod <68671324+Deekshithrathod@users.noreply.github.com>
2022-11-08 20:42:52 +0530

    first commit actual

---
+++ App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Die from "./components/Die";
+import ScoreBoard from "./components/ScoreBoard";
+import { nanoid } from "nanoid";
+import Confetti from "react-confetti";
+
+export default function App() {
+  return <div>This is an app</div>;
+}
